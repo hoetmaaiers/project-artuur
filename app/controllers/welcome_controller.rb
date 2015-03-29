9879a5c248e1803283b03990cc17ca5239de3449
Robin Houdmeyers <houdmeyers@gmail.com>
2015-03-29 21:56:41 +0200

    destroy workshops as separate resource

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+
+  def index
+    @workshops = Workshop.all()
+    @types = ActsAsTaggableOn::Tagging.where(context: 'types').map(&:tag)
+    @audiences = ActsAsTaggableOn::Tagging.where(context: 'audiences').map(&:tag)
+  end
 end
