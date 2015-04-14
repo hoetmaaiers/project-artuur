e2ae35cc259e565848080b79e193e3e4602ccef0
Robin Houdmeyers <houdmeyers@gmail.com>
2015-04-14 22:53:44 +0200

    define Rails.application instead of unkozn 'app'

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,10 +9,10 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # We don't want the default of everything that isn't js or css, because it pulls too many things in
-app.config.assets.precompile.shift
+Rails.application.config.assets.precompile.shift
 
 # Explicitly register the extensions we are interested in compiling
-app.config.assets.precompile.push(Proc.new do |path|
+Rails.application.config.assets.precompile.push(Proc.new do |path|
   File.extname(path).in? [
     '.html', '.erb', '.haml',                 # Templates
     '.png',  '.gif', '.jpg', '.jpeg',         # Images
