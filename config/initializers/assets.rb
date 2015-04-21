23bf9a1dc30589d3234a523ab8fe993363b5e46e
Robin Houdmeyers <houdmeyers@gmail.com>
2015-04-21 22:12:33 +0200

    restore asset precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,17 +5,16 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # We don't want the default of everything that isn't js or css, because it pulls too many things in
-# Rails.application.config.assets.precompile.shift
+Rails.application.config.assets.precompile.shift
 
 # Explicitly register the extensions we are interested in compiling
 Rails.application.config.assets.precompile.push(Proc.new do |path|
   File.extname(path).in? [
     '.html', '.erb', '.haml',                 # Templates
-    # '.png',  '.gif', '.jpg', '.jpeg',         # Images
+    '.png',  '.gif', '.jpg', '.jpeg',         # Images
     '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
   ]
 end)
